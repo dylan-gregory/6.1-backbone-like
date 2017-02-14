769b3c5d2e26a0d9c8e524ecfccda7816d0a8599
dylan-gregory <jdylangregory@gmail.com>
2017-02-14 08:47:50 -0500

    Fixed some code style issues

--- app/scripts/models/like.js
+++ app/scripts/models/like.js
@@ -4,31 +4,31 @@ var Backbone = require('backbone');
 
 
 var Likes = Backbone.Model.extend({
-    defaults: {
-      count: 0,
-      label: "likes"
+      defaults: {
+        count: 0,
+        label: "likes"
     },
     initialize: function(){
       console.log("This button was born!");
     },
     clickCount: function(){
-    var likes = this.get('count');
-        likes += 1;
-        this.set({'count': likes });
+      var likeCount = this.get('count');
+        likeCount += 1;
+        this.set({'count': likeCount });
     },
     updateDisplay: function(){
       $('.btn').html((this.get('count')) + " " + (this.get('label')));
     },
     toJSON: function(){
-    var likes = this.get('count');
-      if (likes == 1) {
-        $('.btn').html((this.get('count')) + " " + ("like"));
-      }else{
-        $('.btn').html((this.get('count')) + " " + (this.get('label')));
-      }
-      return Backbone.Model.prototype.toJSON.call(this);
+      var likeCount = this.get('count');
+        if (likeCount == 1) {
+          $('.btn').html((this.get('count')) + " " + ("like"));
+        }else{
+          $('.btn').html((this.get('count')) + " " + (this.get('label')));
+        }
+        return Backbone.Model.prototype.toJSON.call(this);
 
-    }
+      }
 });
 
 
